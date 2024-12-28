57a73bbe41f529d04248cb1ffc80faf15b3a798a
ArchieTheFox <archiewolf9959@gmail.com>
2024-12-28 21:01:04 +0300

    - started header. - cleaned css a bit.

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+﻿import './App.css'
+
+function Header() {
+    return (
+        <>
+            <ul className="header-menu">
+                <li><a>Test</a></li>
+                <li><a>Test 2</a></li>
+            </ul>
+        </>
+    )
+}
+
+export default Header
